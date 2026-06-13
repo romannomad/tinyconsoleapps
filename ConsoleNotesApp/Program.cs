61915438718f232383aee7c089b3262649b21d76
Roman <63406202+romannomad@users.noreply.github.com>
2026-06-13 09:05:28 +0200

    Update

--- ConsoleNotesApp/Program.cs
+++ ConsoleNotesApp/Program.cs
@@ -4,5 +4,14 @@ using System.IO;
 
 class Program
 {
+    static string notesFilePath = "notes.txt";
 
-}
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Add Note");
+            Console.WriteLine("2. View Notes");
+            Console.WriteLine("3. Delete all notes");
+
+        }
