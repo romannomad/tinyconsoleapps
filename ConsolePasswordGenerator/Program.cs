4108485d0b8bd25df2c124e743020541c2fad874
Roman <63406202+romannomad@users.noreply.github.com>
2026-06-16 18:24:25 +0200

    Update

--- ConsolePasswordGenerator/Program.cs
+++ ConsolePasswordGenerator/Program.cs
@@ -10,6 +10,7 @@ class Program
 
         int length = AskInt("Password length (e.g. 12): ");
         bool includeNumbers = AskBool("Include numbers (y/n): ");
+        bool includeSymbols = AskBool("Include symbols (y/n): ");
 
     }
 }
