fa1ccf3a011e47d4fe3288d55f72af0b69f6900b
Roman <63406202+romannomad@users.noreply.github.com>
2026-06-17 18:33:02 +0200

    Update

--- ConsolePasswordGenerator/Program.cs
+++ ConsolePasswordGenerator/Program.cs
@@ -71,6 +71,7 @@ class Program
         if (password.Length >= 8) score++;
         if (password.Length >= 12) score++;
         if (password.Any(char.IsDigit)) score++;
+        if (password.Any(char.IsUpper)) score++;
 
     }
 
