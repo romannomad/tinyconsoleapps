61a6487f8b5ae7b18d85a0a8a0d8283b63737c62
Roman <63406202+romannomad@users.noreply.github.com>
2026-07-06 21:04:10 +0200

    update

--- HashGenerator/Program.cs
+++ HashGenerator/Program.cs
@@ -37,7 +37,7 @@ class Program
     {
         using HashAlgorithm algorithm = algorithmName switch
         {
-            "MD5" => MD5.Create(),
+            "MD5" => MD5.Create(), // Create an instance of the MD5 hash algorithm
             "SHA256" => SHA256.Create(),
             "SHA512" => SHA512.Create(),
             _ => SHA256.Create()
