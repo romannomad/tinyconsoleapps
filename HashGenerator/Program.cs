b18a8337ec6022096fb01ca4890f8f8473f76e85
Roman <63406202+romannomad@users.noreply.github.com>
2026-07-11 23:06:09 +0200

    up

--- HashGenerator/Program.cs
+++ HashGenerator/Program.cs
@@ -69,7 +69,7 @@ class Program
         using FileStream stream = File.OpenRead(filePath);
         byte[] hashBytes = algorithm.ComputeHash(stream);
 
-        StringBuilder sb = new(); //
+        StringBuilder sb = new(); //    Create a StringBuilder to build the hash string representation
         foreach (byte b in hashBytes) // Iterate through each byte in the hash byte array
         {
             sb.Append(b.ToString("x2")); // Convert each byte to a hexadecimal string and append it to the StringBuilder
