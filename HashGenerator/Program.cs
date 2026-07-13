5df182aac97cc96a62722756fb6c19c0cc031eef
Roman <63406202+romannomad@users.noreply.github.com>
2026-07-13 17:17:55 +0200

    update

--- HashGenerator/Program.cs
+++ HashGenerator/Program.cs
@@ -63,7 +63,7 @@ class Program
             "MD%" => MD5.Create(),//        Create an instance of the MD5 hash algorithm
             "SHA256" => SHA256.Create(),//        Create an instance of the SHA256 hash algorithm  
             "SHA512" => SHA512.Create(),//      Create an instance of the SHA512 hash algorithm
-            _ => SHA256.Create()
+            _ => SHA256.Create() // 
         };
 
         using FileStream stream = File.OpenRead(filePath);
