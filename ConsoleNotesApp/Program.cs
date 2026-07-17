60e19ce91c7879d09190bc0332e67bd7a0151f1a
Roman <63406202+romannomad@users.noreply.github.com>
2026-07-17 23:23:55 +0200

    update

--- ConsoleNotesApp/Program.cs
+++ ConsoleNotesApp/Program.cs
@@ -10,7 +10,7 @@ class Program
     {
         while (true)
         {
-            Console.WriteLine("1. Add Note");
+            Console.WriteLine("1. Add Note");//     
             Console.WriteLine("2. View Notes");
             Console.WriteLine("3. Delete all notes");
             Console.WriteLine("4. Exit");
