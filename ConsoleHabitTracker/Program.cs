953160963447702a6ad805e1a3fa1a175ee7db0f
Roman <63406202+romannomad@users.noreply.github.com>
2026-07-24 23:47:03 +0200

    Update

--- ConsoleHabitTracker/Program.cs
+++ ConsoleHabitTracker/Program.cs
@@ -20,6 +20,9 @@ class Program
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("\n=== Habit Tracker ===");
+            Console.ResetColor();
+
+
 
         }
 
