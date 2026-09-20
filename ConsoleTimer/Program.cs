dca95ac7abf60c3361903e5422e6a5f664e89f85
Roman <63406202+romannomad@users.noreply.github.com>
2026-09-20 07:34:04 +0300

    Update

--- ConsoleTimer/Program.cs
+++ ConsoleTimer/Program.cs
@@ -25,6 +25,8 @@ class Program
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write($"Time left: {i} seconds   ");
             Console.ResetColor();
+
+            Thread.Sleep(1000);
         }
 
     }
