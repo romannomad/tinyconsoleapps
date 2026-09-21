f7cc186ece4dd524be7439bd40826bc8958a820d
Roman <63406202+romannomad@users.noreply.github.com>
2026-09-21 22:38:37 +0300

    Update

--- ConsoleTimer/Program.cs
+++ ConsoleTimer/Program.cs
@@ -9,6 +9,7 @@ class Program
 
         Console.WriteLine("Enter time (examples: 1h 20m 30s, 45m, 120s):");
         Console.Write("Time:");
+        string input =
 
         if (!int.TryParse(Console.ReadLine(), out int seconds) || seconds <= 0)
         {
